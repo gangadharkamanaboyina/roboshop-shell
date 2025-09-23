5827bbeaf4d82e613a6aa38a771d90d016442b47
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-09-23 20:24:03 +0530

    roboshop instance creation

--- roboshop.sh
+++ roboshop.sh
@@ -12,17 +12,18 @@ do
 
   if [[ $instance=="frontend" ]]; then
 
-        IP=$(aws ec2 describe-instances \
+    IP=$(aws ec2 describe-instances \
   --instance-ids $InstanceId \
   --query "Reservations[*].Instances[*].PublicIpAddress" \
   --output text)
 
   else
 
-        IP=$(aws ec2 describe-instances \
+    IP=$(aws ec2 describe-instances \
   --instance-ids $InstanceId \
   --query "Reservations[*].Instances[*].PrivateIpAddress" \
   --output text)
+
   fi
 
   echo "$instance: $IP"
