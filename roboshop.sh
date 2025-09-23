84c583b55561b4b59d0adc3198bc21a24988d702
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-09-23 20:10:21 +0530

    roboshop instance creation

--- roboshop.sh
+++ roboshop.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-for instance in "$@"; do
-    InstanceId=$(aws ec2 run-instances \
-        --image-id ami-09c813fb71547fc4f \
-        --instance-type t3.micro \
-        --security-groups allow-all \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-        --query "Instances[0].InstanceId" \
-        --output text)
+for instance in $@ 
+do
+      InstanceId=$(aws ec2 run-instances \
+  --image-id ami-09c813fb71547fc4f \
+  --instance-type t3.micro \
+  --security-groups allow-all \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+  --query "Instances[0].InstanceId" \
+  --output text)
 
-    if [[ "$instance" == "frontend" ]]; then
-        IP=$(aws ec2 describe-instances \
-            --instance-ids "$InstanceId" \
-            --query "Reservations[*].Instances[*].PublicIpAddress" \
-            --output text)
-    else
-        IP=$(aws ec2 describe-instances \
-            --instance-ids "$InstanceId" \
-            --query "Reservations[*].Instances[*].PrivateIpAddress" \
-            --output text)
-    fi
+  if(($instance=="frontend")); then
 
-    echo "$instance: $IP"
+        IP=$(aws ec2 describe-instances \
+  --instance-ids $InstanceId \
+  --query "Reservations[*].Instances[*].PublicIpAddress" \
+  --output text)
+
+  else
+
+        IP$(aws ec2 describe-instances \
+  --instance-ids $InstanceId \
+  --query "Reservations[*].Instances[*].PrivateIpAddress" \
+  --output text)
+  fi
+
+  echo "$instance: $IP"
+
 done
